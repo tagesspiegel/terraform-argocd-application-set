ecf2804bfa3e32ef3a82aef2fb5e588b1113b0fa
leonsteinhaeuser <leon.steinhaeuser@tagesspiegel.de>
2023-10-27 14:25:09 +0200

    chore: improved env context annotations handling
    Signed-off-by: leonsteinhaeuser <leon.steinhaeuser@tagesspiegel.de>

--- main.tf
+++ main.tf
@@ -63,11 +63,7 @@ resource "argocd_application_set" "this" {
             "application-set" = var.name
           },
           {
-            for k, v in var.env_context_annotations : k => <<EOF
-{{- $applicationName := "${var.name}" }}
-{{- $resourceName := "${local.resource_name}" }}
-{{- $cluster := "" }}{{ if eq (index .Path.Segments 1) "general-purpose" }}{{ $cluster = "in-cluster" }}{{ else }}{{ $cluster = (index .Path.Segments 1) }}{{ end }}${v}
-EOF
+            for k, v in var.env_context_annotations : k => "{{ $applicationName := \"${var.name}\" }}{{ $resourceName := \"${local.resource_name}\" }}{{ $cluster := \"\" }}{{ if eq (index .Path.Segments 1) \"general-purpose\" }}{{ $cluster = \"in-cluster\" }}{{ else }}{{ $cluster = (index .Path.Segments 1) }}{{ end }}${v}"
           }
         )
         labels = merge(

6a59da6afd880112296c26d05da661bc4a8c291b
leonsteinhaeuser <leon.steinhaeuser@tagesspiegel.de>
2024-08-26 16:08:12 +0200

    chore(examples): add example with target_namespace_overwrite
    Signed-off-by: leonsteinhaeuser <leon.steinhaeuser@tagesspiegel.de>

--- examples/main.tf
+++ examples/main.tf
@@ -48,3 +48,55 @@ develop
 EOT
   }
 }
+
+module "example_system" {
+  source = "../"
+
+  name = "example-system"
+  project_name = "example-system"
+  generator = {
+    git = {
+      repo_url = "https://github.com/urbanmedia/example-deployment.git"
+      revision = "main"
+      files = [
+        "argo/*/*/example-system.yaml"
+      ]
+    }
+  }
+  target_namespace_overwrite = "example-system"
+  manifest_source = {
+    helm = {
+      chart = "example-system"
+      release_name = "example-system"
+      repo_url = "ghcr.io/urbanmedia/example-deployment"
+      target_revision = "1.1.16"
+    }
+  }
+  generator_segment_index_overwrite = 1
+  application_name_suffix = null
+  annotations = {
+    "argocd.argoproj.io/sync-wave" = "2",
+  }
+  sync_options = [
+    "Validate=false",
+    "ApplyOutOfSyncOnly=true",
+  ]
+  sync_policy = {
+    prune       = true
+    self_heal   = true
+    allow_empty = true
+  }
+  env_context_annotations = {
+    "argocd-image-updater.argoproj.io/image-list" = "xyz=tagesspiegel/xyz"
+    "argocd-image-updater.argoproj.io/xyz.update-strategy" = "latest"
+    "argocd-image-updater.argoproj.io/xyz.allow-tags" = <<EOT
+{{- if eq $cluster "staging" }}
+regex:v[0-9]{1,}.[0-9]{1,}.[0-9]{1,}-rc.[0-9]{1,}
+{{- else if eq $cluster "production" }}
+regex:v[0-9]{1,}.[0-9]{1,}.[0-9]{1,}
+{{- else }}
+develop
+{{- end }}"
+EOT
+  }
+}
